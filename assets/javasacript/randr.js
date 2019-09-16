0a91f47677737ea1279052533ae605c82914c655
Peter Wu <23109224+PapaWuski@users.noreply.github.com>
2019-09-16 13:42:21 -0700

    created second ajax for recipe info

--- assets/javasacript/randr.js
+++ assets/javasacript/randr.js
@@ -1,22 +1,45 @@
 const obtainRecipe = response => {
   const results = response.results;
+  const ids = [];
   for (let i = 0; i < results.length; i++) {
-    console.log(results[i]);
+    ids.push(results[i].id);
+  }
+  obtainRecipeInfo(results, ids);
+  console.log(ids);
+};
+
+// what i want is (response,results) => {blah blah}
+const obtainRecipeInfo = (recipeResults, ids) => {
+  const url = `https://api.spoonacular.com/recipes/informationBulk?ids=${ids.join(
+    ","
+  )}&apiKey=bb9cefd1ba364505863a1ddb120313da`;
+  $.ajax({
+    url,
+    method: "GET"
+  }).then(function(response) {
+    createCards(recipeResults, response);
+  });
+};
+
+const createCards = (recipeResults, recipes) => {
+  for (let i = 0; i < recipes.length; i++) {
+    console.log(recipes[i]);
     // create jquery object to put all items into body.
-    // PLACEHOLDER is the placeholder text for
-    const $card = $("<div>").addClass("card");
+    // test  is the placeholder text for index.html div
+    const $card = $("<div>").addClass("card recipe");
+    $card.attr("data-id", recipeResults[i].id);
     const $cardImgTop = $("<img>").addClass("card-img-top img-thumbnail");
     $cardImgTop.attr(
       "src",
-      ` https://spoonacular.com/recipeImages/${results[i].id}-636x393.jpg`
+      ` https://spoonacular.com/recipeImages/${recipeResults[i].id}-636x393.jpg`
     );
     const $cardBody = $("<div>").addClass("card-body");
     const $cardTitle = $("<h5>").addClass("card-title");
-    $cardTitle.text(results[i].title);
+    $cardTitle.text(recipeResults[i].title);
     const $cardSubtitle = $("<h6>").addClass("card-subtitle");
-    $cardSubtitle.text(`Serving Size: ${results[i].servings}`);
+    $cardSubtitle.text(`Serving Size: ${recipeResults[i].servings}`);
     const $cardText = $("<p>").addClass("card-text");
-    $cardText.text(`Ready In: ${results[i].readyInMinutes} minutes`);
+    $cardText.text(`Ready In: ${recipeResults[i].readyInMinutes} minutes`);
 
     $cardBody.append($cardTitle, $cardSubtitle, $cardText);
     $card.append($cardImgTop, $cardBody);
@@ -25,9 +48,10 @@ const obtainRecipe = response => {
 };
 
 const snoonacularCalls = event => {
+  event.preventDefault();
   // const food = $("#submit").val().trim();
   const food = "pineapple";
-  const url = `https://api.spoonacular.com/recipes/search?query=${food}&apiKey=bb9cefd1ba364505863a1ddb120313da`;
+  const url = `https://api.spoonacular.com/recipes/search?query=${food}&apiKey=bb9cefd1ba364505863a1ddb120313da&instructionsRequired=true&type=main course`;
 
   $.ajax({
     url,
@@ -35,4 +59,11 @@ const snoonacularCalls = event => {
   }).then(obtainRecipe);
 };
 
+const consoleLogInfo = event => {
+  // event.preventDefault()
+  const $recipe = $(event.target).closest(".recipe");
+  console.log();
+};
+
 $(document).on("click", "#submit", snoonacularCalls);
+$(document).on("click", ".recipe", consoleLogInfo);
