baeb99c7629508fac0aeaa8ce04f61db8243cba4
Peter Wu <23109224+PapaWuski@users.noreply.github.com>
2019-09-19 12:28:47 -0700

    removed filler// changed cards to carousel

--- assets/javasacript/r_and_r_spoontacular.js
+++ assets/javasacript/r_and_r_spoontacular.js
@@ -10,7 +10,6 @@ const obtainRecipe = (food, response) => {
   }
 
   obtainRecipeInfo(food, results, ids);
-
 };
 
 // what i want is (response,results) => {blah blah}
@@ -29,7 +28,6 @@ const obtainRecipeInfo = (food, recipeResults, ids) => {
 const saveToDB = (food, recipeResults, recipes) => {
   let savedRecipes = {};
   for (let i = 0; i < recipes.length; i++) {
-
     savedRecipes[recipeResults[i].id] = [recipeResults[i], recipes[i]];
   }
   database.ref(`/${food}/`).set(savedRecipes);
@@ -43,44 +41,94 @@ const createCards = food => {
     .once("value")
     .then(snapshot => {
       let info = snapshot.val();
+      const $carousel = $("<div>").attr({
+        id: "test",
+        class: "carousel slide",
+        "data-ride": "carousel"
+      });
+      const $prev = $("<a>").attr({
+        class: "carousel-control-prev",
+        href: "#test",
+        role: "button",
+        "data-slide": "prev"
+      });
+      $prev.html(
+        `<span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span>`
+      );
+      const $next = $("<a>").attr({
+        class: "carousel-control-next",
+        href: "#test",
+        role: "button",
+        "data-slide": "next"
+      });
+      $next.html(
+        `<span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span>`
+      );
+      const $carouselIndicator = $("<ol>").addClass("carousel-indicators");
+      const $carouselInner = $("<div>").addClass("carousel-inner");
+      let counter = 0;
+
       for (let item in info) {
         const recipeResults = info[item][0];
         const recipes = info[item][1];
-        const $card = $("<div>").addClass("card recipe mb-2");
-        $card.attr({ "data-id": recipeResults.id, "data-food": food });
-        const $cardImgTop = $("<img>").addClass("card-img-top img-thumbnail");
-        $cardImgTop.attr(
-          "src",
-          ` https://spoonacular.com/recipeImages/${recipeResults.id}-636x393.jpg`
-        );
-        const $cardBody = $("<div>").addClass("card-body");
-        const $cardTitle = $("<h5>").addClass("card-title");
-        $cardTitle.text(recipeResults.title);
-        const $cardSubtitle = $("<h6>").addClass("card-subtitle");
-        $cardSubtitle.text(
+        const $indicator = $("<li>").attr({
+          "data-target": "#test",
+          "data-slide-to": counter
+        });
+
+        const $carouselItem = $("<div>").attr({
+          class: "carousel-item recipe",
+          "data-id": recipeResults.id,
+          "data-food": food
+        });
+
+        const $carImage = $("<img>").addClass("card-img-top img-thumbnail");
+        $carImage.attr({
+          class: "d-block w-100",
+          src: ` https://spoonacular.com/recipeImages/${recipeResults.image}`,
+          alt: recipeResults.title
+        });
+
+        if (!counter) {
+          $indicator.addClass("active");
+          $carouselItem.addClass("active");
+        }
+        const $carInfo = $("<div>").attr({
+          class: "carousel-caption d-none d-md-block bg-info"
+        });
+
+        const $carTitle = $("<h5>").text(recipeResults.title);
+        const $carSubtitle = $("<h6>").text(
           `Price: $${(
             (recipes.pricePerServing * recipeResults.servings) /
             100
           ).toFixed(2)}`
         );
-        const $cardText = $("<p>").addClass("card-text");
-        $cardText.text(`Ready In: ${recipeResults.readyInMinutes} minutes`);
+        const $carText = $("<p>").text(
+          `Ready In: ${recipeResults.readyInMinutes} minutes`
+        );
 
         const $cardRow = $("<div>").addClass("row");
         const $cardColLeft = $("<div>").addClass("col-sm-10");
         const $cardColRight = $("<div>").addClass("col-sm-2");
-        const $cardButton = $("<button>").addClass("fas fa-list-alt btn-lg btn-warning mt-3 getRecipe");
-        $cardButton.attr("data-toggle", "modal")
-        $cardButton.attr("data-target", "#recipeModal")
-        $cardButton.attr("type", "button")
-        
-        $cardColLeft.append($cardTitle, $cardSubtitle, $cardText);
+        const $cardButton = $("<button>").addClass(
+          "fas fa-list-alt btn-lg btn-warning mt-3 getRecipe"
+        );
+        $cardButton.attr("data-toggle", "modal");
+        $cardButton.attr("data-target", "#recipeModal");
+        $cardButton.attr("type", "button");
+
+        $cardColLeft.append($carTitle, $carSubtitle, $carText);
         $cardColRight.append($cardButton);
 
-        $cardBody.append($cardRow.append($cardColLeft, $cardColRight));
-        $card.append($cardImgTop, $cardBody);
-        $("#recipieList").append($card);
+        $carInfo.append($cardRow.append($cardColLeft, $cardColRight));
+
+        counter++;
+        $carouselIndicator.append($indicator);
+        $carouselInner.append($carouselItem.append($carImage, $carInfo));
       }
+      $carousel.append($carouselIndicator, $carouselInner, $prev, $next);
+      $("#recipieList").append($carousel);
     });
 };
 
@@ -119,41 +167,45 @@ const consoleLogInfo = event => {
   const id = $recipe.attr("data-id");
   const food = $recipe.attr("data-food");
   database
-  .ref(`/${food}/${id}/1`)
-  .once("value")
-  .then(snapshot => {
-    const info = snapshot.val();
-    console.log(info)
-    //name of the recipe 
+    .ref(`/${food}/${id}/1`)
+    .once("value")
+    .then(snapshot => {
+      const info = snapshot.val();
+      console.log(info);
+      //name of the recipe
 
-    //ingredients
-    const ingredientsList = [];
-    const stepList =[];
-    info.extendedIngredients.forEach(ingredient => {
+      //ingredients
+      const ingredientsList = [];
+      const stepList = [];
+      info.extendedIngredients.forEach(ingredient => {
+        ingredientsList.push(`${ingredient.original}`);
+        // ingredientsList.push(`${ingredient.amount} ${ingredient.unit} ${ingredient.name}`)
+      });
+      //steps
+      info.analyzedInstructions[0].steps.forEach((step, index) => {
+        stepList.push(`Step ${index + 1}: ${step.step}`);
+      });
 
-      ingredientsList.push(`${ingredient.original}`)
-      // ingredientsList.push(`${ingredient.amount} ${ingredient.unit} ${ingredient.name}`)
-    })
-    //steps
-    info.analyzedInstructions[0].steps.forEach((step,index) => {
-      stepList.push(`Step ${index+1}: ${step.step}`)
-    })
+      console.log(info.title);
+      console.log(ingredientsList);
+      console.log(stepList);
+      console.log(info.sourceUrl);
 
-    console.log(info.title)
-    console.log(ingredientsList)
-    console.log(stepList)
-    console.log(info.sourceUrl)
+      $("#recipeModalTitle").text(info.title);
 
-    $('#recipeModalTitle').text(info.title);
-
-    for (let i = 0; i < ingredientsList.length; i++){
-      $('#ingredients').append($('<p>').addClass("font-weight-light mb-2").text(ingredientsList[i])).append($('<hr>'));
-    };
-    for (let i = 0; i < stepList.length; i++){
-      $('#instructions').append($('<p>').text(stepList[i]));
-    };
-  });
-
+      for (let i = 0; i < ingredientsList.length; i++) {
+        $("#ingredients")
+          .append(
+            $("<p>")
+              .addClass("font-weight-light mb-2")
+              .text(ingredientsList[i])
+          )
+          .append($("<hr>"));
+      }
+      for (let i = 0; i < stepList.length; i++) {
+        $("#instructions").append($("<p>").text(stepList[i]));
+      }
+    });
 };
 
 $(document).on("click", ".searchClick", event => {
@@ -177,7 +229,6 @@ $(".initSearchClick").on("click", event => {
     return;
   }
 
-
   $("#searchTarget").text(`You're looking for "${foodInput}"`);
   snoonacularCalls();
   displayRestaraunts();
@@ -190,4 +241,3 @@ $(".searchField").keyup(function(event) {
     $(".initSearchClick").click();
   }
 });
-
